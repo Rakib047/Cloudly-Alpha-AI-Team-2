72c370f9b2b4027f7e5f1b8d9c9af19b10f9ca2a
Nuha <nusratjahannuha95@gmail.com>
2025-06-02 14:37:38 +0600

    removed project directory

--- Test Driven Development/TDD-Project/TDD-Project-Nuha/src/Book_Class.py
+++
@@ -1,9 +0,0 @@
-class Book:
-    def __init__(self, title="Unknown", quantity=0, author="Unknown", price=0.0):
-        self.title = title
-        self.quantity = quantity
-        self.author = author
-        self.price = price
-
-    def __repr__(self):
-        return f"Book: {self.title}, Quantity: {self.quantity}, Author: {self.author}, Price: {self.price}"
